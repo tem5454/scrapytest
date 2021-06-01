df503b9c95b3ff29a31f7beeb5bcb497687f1689
DESKTOP-MJGA1UJ\tem54 <tem5454@naver.com>
2021-06-01 22:03:34 +0900

    commit

--- scrapytest/spiders/amazon.py
+++ scrapytest/spiders/amazon.py
@@ -8,11 +8,51 @@ from scrapytest.items import ScrapytestItem
 class AmazonSpider(scrapy.Spider):
     name = 'amazon'
     allowed_domains = ['amazon.com']
-    start_urls = ['http://amazon.com/']
+    
+    cookies = {
+        "anonymid": "j7wsz80ibwp8x3",
+        "_r01_": "1",
+        "ln_uact": "mr_mao_hacker@163.com",
+        "_de": "BF09EE3A28DED52E6B65F6A4705D973F1383380866D39FF5",
+        "depovince": "GW",
+        "jebecookies": "2fb888d1-e16c-4e95-9e59-66e4a6ce1eae|||||",
+        "ick_login": "1c2c11f1-50ce-4f8c-83ef-c1e03ae47add",
+        "p": "158304820d08f48402be01f0545f406d9",
+        "first_login_flag": "1",
+        "ln_hurl": "http://hdn.xnimg.cn/photos/hdn521/20180711/2125/main_SDYi_ae9c0000bf9e1986.jpg",
+        "t": "adb2270257904fff59f082494aa7f27b9",
+        "societyguester": "adb2270257904fff59f082494aa7f27b9",
+        "id": "327550029",
+        "xnsid": "4a536121",
+        "loginfrom": "syshome",
+        "wp_fold": "0"
+    }
+
+    headers = {
+        'Host': 'www.amazon.com',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 7.0; \
+                        SM-A520F Build/NRD90M; wv) AppleWebKit/537.36 \
+                        (KHTML, like Gecko) Version/4.0 \
+                        Chrome/65.0.3325.109 Mobile Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,\
+                        application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    }
+
+    def start_requests(self):
+        start_urls = ['http://amazon.com/']
+
+        for url in start_urls:
+            print('==============================================================')
+            print(url)
+            print('==============================================================')
+            yield scrapy.Request(url, headers=self.headers, callback=self.parse)
+        
 
     def parse(self, response):
-        test = response.xpath('//*[@id="nav-your-amazon-text"]')
-        
+        test = response.xpath('//*[@id="nav-your-amazon-text"]//text()')
+        print('==============================================================')
+        print(response.source)
+        print('==============================================================')
 
         item = ScrapytestItem()
         item['test'] = test.extract()
